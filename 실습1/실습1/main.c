23970ece9f7f883728d37d1fe1180f37443a7e91
seojeewon <jw07061@ewhain.net>
2023-03-27 12:29:00 +0900

    overall code

--- 실습1/실습1/main.c
+++ 실습1/실습1/main.c
@@ -6,6 +6,12 @@
 #define STsize 1000 //size of string table
 #define HTsize 100 //size of hash table
 
+#define FALSE 0
+#define TRUE 1
+
+#define isLetter(x) (((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z') || ((x) == '_'))
+#define isDigit(x) ((x) >= '0' && (x) <= '9')
+
 typedef struct HTentry* HTpointer;
 typedef struct HTentry {
 	int index; //index of identifier in ST
@@ -14,13 +20,26 @@ typedef struct HTentry {
 
 enum errorTypes { noerror, illsp, illid, overst };
 typedef enum errorTypes ERRORtypes;
+
 char seperators[] = " .,;:?!\t\n";
+
 HTpointer HT[HTsize];
 char ST[STsize];
 
+int nextid = 0;	//the current identifier
+int nextfree = 0;	//the next available index of ST
+int hashcode;	//hash code of identifier
+int sameid;	//first index of identifier
+
+int found;	//for the previous occurrence of an identifier
+
 ERRORtypes err;
+
 FILE* fp; //to be a pointer to FILE
 char input;
+
+
+
 //Initialize - open input file
 void initialize()
 {
@@ -28,11 +47,23 @@ void initialize()
 	input = fgetc(fp);
 }
 
-/* 지원
-   Skip Seperators - skip over strings of spaces, tabs, newlines, ., ; : ? !
-   if illegal seperators,print out error message. */
-void SkipSeperators()
-{
+int isSeperator(char c) {
+	int sep_len;
+
+	sep_len = strlen(seperators);
+	for (int i = 0; i < sep_len; i++) {
+		if (c == seperators[i])
+			return 1;
+	}
+	return 0;
+}
+
+void PrintHeading() {
+	printf("\n\n");
+	printf(" -----------         -----------\n");
+	printf(" Index in ST            identifier \n");
+	printf(" -----------         -----------\n");
+	printf("\n");
 }
 
 /* 서영
@@ -41,6 +72,22 @@ void SkipSeperators()
    Print out the number of characters used up in ST.*/
 void PrintHStable()
 {
+	HTpointer here;
+
+	printf("\n\n\n\n\n [[ HASH TABLE ]] \n\n");
+
+	for (int i = 0; i < HTsize; i++) {
+		if (HT[i] != NULL) {
+			printf(" Hash Code %3d : ", i);
+			for (here = HT[i]; here != NULL; here = here->next) {
+				int j = here->index;
+				while (ST[j] != '\0' && j < STsize)
+					printf("%c", ST[j++]);
+				printf("  ");
+			}
+		}
+	}
+	printf("\n\n\n < %5d characters are used in the string table >\n", nextfree);
 }
 
 /* 효진
@@ -51,6 +98,37 @@ void PrintHStable()
    illsp :illegal seperator*/
 void PrintError(ERRORtypes err)
 {
+	switch (err) {
+		case overst:
+			printf("...Error...  OVERFLOW ");
+			PrintHStable();
+			exit(0);
+			break;
+		case illsp:
+			printf("...Error...  %c is illegal seperator \n", input);
+			break;
+		case illid:
+			printf("...Error...");
+			while (input != EOF && (isLetter(input) || isDigit(input))) {
+				printf("%c", input);
+				input = fgetc(fp);
+			}
+			printf(" start with digit \n");
+			break;
+	}
+}
+
+/* 지원
+   Skip Seperators - skip over strings of spaces, tabs, newlines, ., ; : ? !
+   if illegal seperators,print out error message. */
+void SkipSeperators() {
+	while (input != EOF && !(isLetter(input) || isDigit(input))) {
+		if (!isSeperator(input)) {
+			err = illsp;
+			PrintError(err);
+		}
+		input = fgetc(fp);
+	}
 }
 
 /* 지원
@@ -60,6 +138,21 @@ void PrintError(ERRORtypes err)
    If first letter is digit, print out error message. */
 void ReadID()
 {
+	nextid = nextfree;
+	if (isDigit(input)) {
+		err = illid;
+		PrintError(err);
+	}
+	else {
+		while (input != EOF && (isLetter(input) || isDigit(input))) {
+			if (nextfree == STsize) {
+				err = overst;
+				PrintError(err);
+			}
+			ST[nextfree++] = input;
+			input = fgetc(fp);
+		}
+	}
 }
 
 /* 서영
@@ -67,6 +160,12 @@ void ReadID()
    characters and then taking the sum modulo the size of HT. */
 void ComputeHS(int nid, int nfree)
 {
+	int code;
+	code = 0;
+	for (int i = nid; i < nfree - 1; i++) {
+		code += (int)ST[i];
+	}
+	hashcode = code % HTsize;
 }
 
 /* 효원
@@ -76,6 +175,29 @@ void ComputeHS(int nid, int nfree)
    If find a match, save the starting index of ST in same id. */
 void LookupHS(int nid, int hscode)
 {
+	HTpointer here;
+	int i, j;
+
+	found = FALSE;
+	if (HT[hscode] != NULL) {
+		here = HT[hscode];
+		while (here != NULL && found == FALSE) {
+			found = TRUE;
+			i = here->index;
+			j = nid;
+			sameid = i;
+
+			while (ST[i] != '\0' && ST[j] != '\0' && found == TRUE) {
+				if (ST[i] != ST[i])
+					found = FALSE;
+				else {
+					i++;
+					j++;
+				}
+			}
+			here = here->next;
+		}
+	}
 }
 
 /* 효원
@@ -85,6 +207,12 @@ void LookupHS(int nid, int hscode)
    IF list head is not a null , it adds a new identifier to the head of the chain */
 void ADDHT(int hscode)
 {
+	HTpointer ptr;
+
+	ptr = (HTpointer)malloc(sizeof(ptr));
+	ptr->index = nextid;
+	ptr->next = HT[hscode];
+	HT[hscode] = ptr;
 }
 
 /* 효진
@@ -108,7 +236,8 @@ int main() {
 		ReadID();
 		if (input != EOF && err != illid) {
 			if (nextfree == STsize) {
-				//print error message
+				err = overst;
+				PrintError(err);
 			}
 			ST[nextfree++] = '\0';
 
@@ -116,15 +245,21 @@ int main() {
 			LookupHS(nextid, hashcode);
 
 			if (!found) {
-				//print message
+				printf("%6d      ", nextid);
+				for (i = nextid; i < nextfree - 1; i++) {
+					printf("%c", ST[i]);
+				}
+				printf("       (entered)\n");
 				ADDHT(hashcode);
 			}
 			else {
-				//print message
+				printf("%6d      ", sameid);
+				for (i = nextid; i < nextfree; i++)
+					printf("%c", ST[i]);
+				printf("       (already existed)\n");
+				nextfree = nextid;
 			}
 		}
-		PrintHStable();
 	}
-
-	return 0;
+	PrintHStable();
 }
